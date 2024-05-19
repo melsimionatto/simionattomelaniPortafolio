cc251188772cd893b321ef31968fd114eef6cb53
melsimionatto <simionattomelani@gmail.com>
2024-05-19 14:12:23 -0300

    primer commit

---
+++ img/script.js
@@ -0,0 +1,20 @@
+@-webkit-keyframes slideDown  {
+    0% {
+      -webkit-transform: translate3d(0, -100%, 0); }
+    100% {
+      -webkit-transform: none; } }
+  @-moz-keyframes slideDown {
+    0% {
+      -moz-transform: translate3d(0, -100%, 0); }
+    100% {
+      -moz-transform: none; } }
+  @-o-keyframes slideDown {
+    0% {
+      -o-transform: translate3d(0, -100%, 0); }
+    100% {
+      -o-transform: none; } }
+  @keyframes slideDown {
+    0% {
+      transform: translate3d(0, -100%, 0); }
+    100% {
+      transform: none; } }
